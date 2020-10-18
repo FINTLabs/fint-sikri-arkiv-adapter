6dc2d4b94e63ba1f9f81a4fb6e20b660368a572b
Frode Sjovatsen <frode@fintlabs.com>
2020-10-18 14:07:30 +0200

    AD-10 AD-18 #comment Added handlers and data services for drosjeløyve

---
+++ src/main/java/no/fint/sikri/data/drosjeloyve/DrosjeloyveService.java
@@ -0,0 +1,9 @@
+package no.fint.sikri.data.drosjeloyve;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class DrosjeloyveService {
+}
---
+++ src/main/java/no/fint/sikri/handler/drosjeloyve/UpdateDrosjeloyveHandler.java
@@ -0,0 +1,19 @@
+package no.fint.sikri.handler.drosjeloyve;
+
+import no.fint.event.model.Event;
+import no.fint.model.resource.FintLinks;
+import no.fint.sikri.handler.Handler;
+
+import java.util.Set;
+
+public class UpdateDrosjeloyveHandler implements Handler {
+    @Override
+    public void accept(Event<FintLinks> fintLinksEvent) {
+
+    }
+
+    @Override
+    public Set<String> actions() {
+        return null;
+    }
+}
