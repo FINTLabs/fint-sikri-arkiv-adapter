7304f461db825bfab10187aab6abd06fdb866b15
Asgeir Storesund Nilsen <asgeir@fintlabs.no>
2021-02-09 08:28:13 +0100

    Refactor ValidationService to handle collections. (#15)

--- src/main/java/no/fint/sikri/handler/noark/CreateDokumentfilHandler.java
+++ src/main/java/no/fint/sikri/handler/noark/CreateDokumentfilHandler.java
@@ -20,6 +20,7 @@ import org.springframework.stereotype.Service;
 import java.util.Collections;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 @Slf4j
@@ -46,7 +47,7 @@ public class CreateDokumentfilHandler implements Handler {
         }
         DokumentfilResource dokumentfilResource = objectMapper.convertValue(response.getData().get(0), DokumentfilResource.class);
 
-        List<Problem> problems = validationService.getProblems(dokumentfilResource);
+        List<Problem> problems = validationService.getProblems(dokumentfilResource).collect(Collectors.toList());
         if (!problems.isEmpty()) {
             response.setResponseStatus(ResponseStatus.REJECTED);
             response.setMessage("Payload fails validation!");
