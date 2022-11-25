65c41945d0067f968338ae8f4562773537d7c786
Nils-Odd Solberg <nilsodd@fintlabs.no>
2022-11-25 16:12:33 +0100

    ARKIV-24 How 'bout some ant matchers (and debug security logging)?

---
+++ src/main/java/no/fint/sikri/config/SecurityConfig.java
@@ -0,0 +1,16 @@
+package no.fint.sikri.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.debug(true);
+
+        web.ignoring().antMatchers("/**");
+    }
+}
