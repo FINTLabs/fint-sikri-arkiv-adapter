ddaa90016dac40b053fa70aceb526b032b300c32
Nils-Odd Solberg <nilsodd@fintlabs.no>
2022-11-25 17:00:37 +0100

    ARKIV-24 More ant matchers.

--- src/main/java/no/fint/sikri/config/SecurityConfig.java
+++ src/main/java/no/fint/sikri/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package no.fint.sikri.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -9,8 +10,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(WebSecurity web) throws Exception {
-        web.debug(true);
-
+        web.debug(false);
         web.ignoring().antMatchers("/**");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.authorizeRequests().antMatchers("/**").permitAll();
     }
 }
