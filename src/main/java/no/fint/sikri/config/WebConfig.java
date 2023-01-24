8abcc708d6b9a20dbff242a35417aaab4137a6b3
Nils-Odd Solberg <nilsodd@fintlabs.no>
2023-01-24 15:52:54 +0100

    ARKIV-24 😭

--- src/main/java/no/fint/sikri/config/WebConfig.java
+++ src/main/java/no/fint/sikri/config/WebConfig.java
@@ -5,7 +5,7 @@ import org.springframework.util.AntPathMatcher;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-@Configuration
+//@Configuration
 public class WebConfig extends WebMvcConfigurerAdapter {
 
     @Override
