4e10d36b76c83e8a4625bc539ef2f043f3e1ebb2
Nils-Odd Solberg <33060326+nilsodd@users.noreply.github.com>
2023-02-13 15:51:02 +0100

    See you later, validator 🎶 (#70)
    * Downgrading hibarnate.validator
    
    * Let`s test the validator
    
    ---------
    
    Co-authored-by: Ole Anders Eidjord <ole.anders.eidjord@vigoiks.no>

---
+++ src/test/groovy/no/fint/sikri/service/ValidationServiceSpec.groovy
@@ -0,0 +1,36 @@
+package no.fint.sikri.service
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import no.fint.event.model.Event
+import no.fint.model.arkiv.kulturminnevern.KulturminnevernActions
+import no.fint.model.resource.FintLinks
+import no.fint.model.resource.arkiv.kulturminnevern.TilskuddFartoyResource
+import spock.lang.Specification
+
+import javax.validation.Validation
+import javax.validation.ValidatorFactory
+
+class ValidationServiceSpec extends Specification {
+    private ValidatorFactory validatorFactory
+
+    void setup(){
+        validatorFactory = Validation.buildDefaultValidatorFactory()
+
+    }
+
+    def "Let`s validate"() {
+        given:
+        ValidationService service = new ValidationService(validatorFactory: validatorFactory)
+        Event<FintLinks> event = new Event<FintLinks>('test.no', 'test', KulturminnevernActions.GET_TILSKUDDFREDABYGNINGPRIVATEIE, 'test')
+
+        def om = new ObjectMapper()
+        def resource = om.readValue("{}", TilskuddFartoyResource)
+
+        when:
+        boolean result = service.validate(event, resource)
+
+        then:
+        result == false
+    }
+
+}
