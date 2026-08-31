fc6ebbc46fdc4a751d098405d5c5758f26b8aade
Nils-Odd Solberg <nilsodd@fintlabs.no>
2026-08-31 13:48:56 +0200

    Logs4Dates

--- src/main/java/no/novari/fint/sikri/data/noark/journalpost/JournalpostFactory.java
+++ src/main/java/no/novari/fint/sikri/data/noark/journalpost/JournalpostFactory.java
@@ -78,6 +78,14 @@ public class JournalpostFactory {
         journalpost.setOpprettetDato(result.getCreatedDate().toGregorianCalendar().getTime());
         journalpost.setJournalDato(result.getRegistryDate().toGregorianCalendar().getTime());
         optionalValue(result.getDocumentDate()).map(XmlUtils::javaDate).ifPresent(journalpost::setDokumentetsDato);
+
+        if (log.isTraceEnabled()) {
+            log.trace("OpprettetDato just set to: {}, JournalDato: {} and DokumentetsDato: {}",
+                    journalpost.getOpprettetDato(), journalpost.getJournalDato(), journalpost.getDokumentetsDato());
+            log.trace("All dates based on the RegistryEntryType values {} (CreatedDate), {} (RegistryDate) and {} (DocumentDate)",
+                    result.getCreatedDate(), result.getRegistryDate(), result.getDocumentDate());
+        }
+
         journalpost.setJournalAr(String.valueOf(result.getRegisterYear()));
         journalpost.setJournalPostnummer(Long.valueOf(result.getDocumentNumber()));
         // TODO journalpost.setJournalSekvensnummer(Long.valueOf(result.getSequenceNumber()));
@@ -152,6 +160,9 @@ public class JournalpostFactory {
         applyParameter(journalpostResource.getJournalDato(), registryEntry::setRegistryDate, xmlUtils::xmlDate);
 
         applyParameter(journalpostResource.getDokumentetsDato(), registryEntry::setDocumentDate, xmlUtils::xmlDate);
+        if(log.isTraceEnabled()) {
+            log.trace("Registry Entry set document date to: {}", journalpostResource.getDokumentetsDato());
+        }
 
         applyParameterFromLink(journalpostResource.getJournalposttype(), registryEntry::setRegistryEntryTypeId);
 
